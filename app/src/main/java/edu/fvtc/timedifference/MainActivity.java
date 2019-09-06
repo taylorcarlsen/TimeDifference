9905d368553cb1b6f7bd1233bc3f6a3a7e9e9fa7
taylorcarlsen <taylorcarlsen@users.noreply.github.com>
2019-09-06 11:37:17 -0500

    FInished
    Finished project

--- app/src/main/java/edu/fvtc/timedifference/MainActivity.java
+++ app/src/main/java/edu/fvtc/timedifference/MainActivity.java
@@ -53,81 +53,84 @@ public class MainActivity extends AppCompatActivity implements CompoundButton.On
                 int endSecondsFromMidnight;
 
                 //checks start input and makes seconds
-                Date formattedStart;
                 try {
-                    formattedStart = simpleDateFormat.parse(etStart.getText().toString());
-                    String stringStart = simpleDateFormat.format(formattedStart);
+                    String startString = etStart.getText().toString();
 
                     try{
-                        hour = Integer.parseInt(stringStart.substring(0,2));
-                        minute = Integer.parseInt(stringStart.substring(3,5));
-                        second = Integer.parseInt(stringStart.substring(6));
+                        hour = Integer.parseInt(startString.substring(0,2));
+                        minute = Integer.parseInt(startString.substring(3,5));
+                        second = Integer.parseInt(startString.substring(6));
 
-                        startSecondsFromMidnight = (hour * 3600) + (minute * 60) + second;
+                        if(hour < 24 && minute < 60 && second <60){
+                            startSecondsFromMidnight = (hour * 3600) + (minute * 60) + second;
 
-                        //Checks end input and makes seconds
-                        Date formattedEnd;
-                        try {
-                            formattedEnd = simpleDateFormat.parse(etEnd.getText().toString());
-                            String stringEnd = simpleDateFormat.format(formattedEnd);
+                            //Checks end input and makes seconds
+                            try {
+                                String endString = etEnd.getText().toString();
 
-                            try{
-                                hour = Integer.parseInt(stringEnd.substring(0,2));
-                                minute = Integer.parseInt(stringEnd.substring(3,5));
-                                second = Integer.parseInt(stringEnd.substring(6));
+                                try{
+                                    hour = Integer.parseInt(endString.substring(0,2));
+                                    minute = Integer.parseInt(endString.substring(3,5));
+                                    second = Integer.parseInt(endString.substring(6));
 
-                                endSecondsFromMidnight = (hour * 3600) + (minute * 60) + second;
+                                    if(hour < 24 && minute < 60 && second <60) {
+                                        endSecondsFromMidnight = (hour * 3600) + (minute * 60) + second;
 
-                                if(endSecondsFromMidnight > startSecondsFromMidnight){
+                                        if (endSecondsFromMidnight > startSecondsFromMidnight) {
 
-                                    int difference = endSecondsFromMidnight - startSecondsFromMidnight;
-                                    int p1 = difference % 60;
-                                    int p2 = difference / 60;
-                                    int p3 = p2 % 60;
-                                    p2 = p2 / 60;
-                                    String sP1;
-                                    String sP2;
-                                    String sP3;
+                                            int difference = endSecondsFromMidnight - startSecondsFromMidnight;
+                                            int p1 = difference % 60;
+                                            int p2 = difference / 60;
+                                            int p3 = p2 % 60;
+                                            p2 = p2 / 60;
+                                            String sP1;
+                                            String sP2;
+                                            String sP3;
 
-                                    if(p1 < 10) {
-                                        sP1 = "0" + String.valueOf(p1);
+                                            if (p1 < 10) {
+                                                sP1 = "0" + String.valueOf(p1);
+                                            } else {
+                                                sP1 = String.valueOf(p1);
+                                            }
+                                            if (p2 < 10) {
+                                                sP2 = "0" + String.valueOf(p2);
+                                            } else {
+                                                sP2 = String.valueOf(p2);
+                                            }
+                                            if (p3 < 10) {
+                                                sP3 = "0" + String.valueOf(p3);
+                                            } else {
+                                                sP3 = String.valueOf(p3);
+                                            }
+
+                                            tvCalculation.setText(valueOf(difference) + " " + "=" + " " + sP2 + ":" + sP3 + ":" + sP1);
+
+                                            Log.d(TAG, "Successful input to user.");
+                                        } else {
+                                            Toast.makeText(MainActivity.this, "End time must be later than start time.", Toast.LENGTH_LONG).show();
+                                        }
                                     }else{
-                                        sP1 = String.valueOf(p1);
-                                    }
-                                    if(p2 < 10){
-                                        sP2 = "0" + String.valueOf(p2);
-                                    }else{
-                                        sP2 = String.valueOf(p2);
-                                    }
-                                    if(p3 < 10){
-                                        sP3 = "0" + String.valueOf(p3);
-                                    }else{
-                                        sP3 = String.valueOf(p3);
+                                        Log.d(TAG, "End input time was not correct.");
+                                        Toast.makeText(MainActivity.this, "End time is not an actual 24 hour time.", Toast.LENGTH_LONG).show();
                                     }
 
-                                    tvCalculation.setText(valueOf(difference) + " " + "=" + " " + sP2 + ":" + sP3 + ":" + sP1);
-
-                                    Log.d(TAG, "Successful input to user.");
+                                }catch(Exception e) {
+                                    Log.d(TAG,"Could not parse times");
                                 }
-                                else{
-                                    Toast.makeText(MainActivity.this, "End time must be later than start time.", Toast.LENGTH_LONG).show();
-                                }
 
-
-                            }catch(Exception e) {
-                                Log.d(TAG,"Could not parse times");
+                            } catch (Exception e) {
+                                Log.d(TAG, "Could not parse start.");
+                                Toast.makeText(MainActivity.this, "Please use format 00:00:00 on time start.", Toast.LENGTH_LONG).show();
                             }
-
-                        } catch (ParseException e) {
-                            Log.d(TAG, "Could not parse start.");
-                            Toast.makeText(MainActivity.this, "Please use format 00:00:00 on time start.", Toast.LENGTH_LONG).show();
+                        }else{
+                            Log.d(TAG, "Start input time was not correct.");
+                            Toast.makeText(MainActivity.this, "Start time is not an actual 24 hour time.", Toast.LENGTH_LONG).show();
                         }
-
                     }catch(Exception e) {
                         Log.d(TAG,"Could not parse times");
                     }
 
-                } catch (ParseException e) {
+                } catch (Exception e) {
                     Log.d(TAG, "Could not parse start.");
                     Toast.makeText(MainActivity.this, "Please use format 00:00:00 on time start.", Toast.LENGTH_LONG).show();
                 }
